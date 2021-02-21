dde901f342c362fac44091c766dd51ecd4e5e2e5
abhipaiangle17 <abhiphotoholic@gmail.com>
2021-02-21 23:43:27 +0530

    Updates

---
+++ precache-manifest.a0aedc2c5878ce9519dbbd4599398b00.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "098127e84bfdcf22ed00ab9c654ee228",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bd5b126130c81980e9a",
+    "url": "/static/css/main.eae38a24.chunk.css"
+  },
+  {
+    "revision": "1dacd79567bc014d9318",
+    "url": "/static/js/2.1a9aec79.chunk.js"
+  },
+  {
+    "revision": "a09bcd6464a11c77a5caeae57fde5da8",
+    "url": "/static/js/2.1a9aec79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bd5b126130c81980e9a",
+    "url": "/static/js/main.2bd0f8db.chunk.js"
+  },
+  {
+    "revision": "2903eb1f4c21d3b2e2e5",
+    "url": "/static/js/runtime-main.89ba0b7d.js"
+  },
+  {
+    "revision": "c52465ef9caad3880a1d49ab4454abfb",
+    "url": "/static/media/map.c52465ef.png"
+  },
+  {
+    "revision": "d0f475896ae2ba64ffdf69cf510e9965",
+    "url": "/static/media/profile_photo.d0f47589.jpg"
+  },
+  {
+    "revision": "53e9bdf1b406c77a4103de5bdcfd1022",
+    "url": "/static/media/project2.53e9bdf1.png"
+  },
+  {
+    "revision": "f497837b446e11890944df4d3def4772",
+    "url": "/static/media/project3.f497837b.png"
+  },
+  {
+    "revision": "9411057e4274585081eddca5da4f39b0",
+    "url": "/static/media/project4.9411057e.png"
+  }
+]);
