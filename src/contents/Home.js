c4be7e64a3ef427139aa2f02e23de8d49efe60a3
abhipaiangle17 <abhiphotoholic@gmail.com>
2021-10-31 23:36:55 +0530

    Updated Home and Bio Page

--- src/contents/Home.js
+++ src/contents/Home.js
@@ -8,8 +8,8 @@ class Home extends Component {
     render() {
         return (
             <div className="condiv home">
-            <h1>Abhishek Pai Angle</h1>
-            <ReactTypingEffect className="typingeffect" text={['A Full Stack Developer','A Deep Learning Enthusiast']} speed={100} eraseDelay={900}/>
+            <h1> Abhishek Pai Angle </h1>
+            <ReactTypingEffect className="typingeffect" text={['Full Stack Developer','Deep Learning Enthusiast']} speed={100} eraseDelay={900}/>
             <div className="hr_line glass"><div className="hr_inner"></div></div>
             <Social />
             </div>
--- src/contents/Portfolio.js
+++ src/contents/Portfolio.js
@@ -8,7 +8,7 @@ class Portfolio extends Component {
     render() {
         return (
             <div className="condiv">
-            <h1 className="subtopic">My Work</h1>
+            <h1 className="subtopic">My Projects</h1>
             <div className="hr_line line2 glass"><div className="inner2 inner3"></div></div>
             <div class="port_out port1 glass"><a href="https://drive.google.com/drive/folders/1wTkf9lydWdtbvhunMsn-tRUhUtOuc0dS?usp=sharing"><img src={project1} className="po1"></img></a></div>
             <div class="port_out port2 glass"><a href="https://github.com/abhipaiangle/pi-chat"><img src={project2} className="po1"></img></a></div>
